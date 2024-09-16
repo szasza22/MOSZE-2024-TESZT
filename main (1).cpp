2fa9e70f0b88f6163fe75b73c1bc612aea46c211
szasza22 <szalayszabolcs2001@gmail.com>
2024-09-16 12:01:51 +0200

    Hibák leírása

--- main (1).cpp
+++ main (1).cpp
@@ -4,23 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // Alsó vonal hiánya
+    std::cout << '1-100 ertekek duplazasa' // Itt aposztrof helyett idézőjel kell
+    for (int i = 0;) // itt hiányos a ciklusfeltétel
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // Hibás ciklusfelvétel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // Pontos vessző hiánya
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+        for (int i = 0; i < N_ELEMENTS, i++) // Vessző helyett pontos vessző
     {
-        atlag += b[i]
+        atlag += b[i] //Itt hiányzik egy pontosvessző
     }
-    atlag /= N_ELEMENTS;
+     atlag /= N_ELEMENTS; //a változó int lesz, viszont átlagnak double vagy float kellene
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
+
